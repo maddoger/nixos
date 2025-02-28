8d1571cd620aedf278b6ce2abc37035978702226
Vitaliy Syrchikov <maddoger@gmail.com>
2025-02-28 12:52:59 +0400

    feat: add fractional scale to gnome

--- hosts/common/optional/gnome.nix
+++ hosts/common/optional/gnome.nix
@@ -3,6 +3,11 @@
     xserver = {
       desktopManager.gnome = {
         enable = true;
+        extraGSettingsOverridePackages = [ pkgs.mutter ];
+        extraGSettingsOverrides = ''
+          [org.gnome.mutter]
+          experimental-features=['scale-monitor-framebuffer']
+        '';
       };
       displayManager.gdm = {
         enable = true;
