b40ccb2ff42aaf65d31901b8f2208c8c3758a10f
Vitaliy Syrchikov <maddoger@gmail.com>
2025-03-02 13:29:03 +0400

    fix: fix kde settings

--- hosts/common/optional/kde.nix
+++ hosts/common/optional/kde.nix
@@ -5,12 +5,12 @@
   services = {
     xserver = {
       enable = true;
-      displayManager.sddm = {
-        enable = true;
-      };
-      desktopManager.plasma6 = {
-        enable = true;
-      };
+    };
+    displayManager.sddm = {
+      enable = true;
+    };
+    desktopManager.plasma6 = {
+      enable = true;
     };
   };
 }
